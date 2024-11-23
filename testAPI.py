e462a69dbbc9c373e89e5e89207222c745e668d2
andre-bae <andre.bae@gmail.com>
2024-11-23 23:50:27 +0300

    список всех криптовалют

--- testAPI.py
+++ testAPI.py
@@ -33,6 +33,38 @@ def get_bitcoin_price():
         mb.showerror("Ошибка", f"Произошла ошибка при выполнении запроса: {e}")
 
 
+def get_all_coins() -> List[Dict]:
+    # URL для получения списка всех криптовалют
+    url = "https://api.coingecko.com/api/v3/coins/list"
+
+    try:
+        # Выполнить GET-запрос к API CoinGecko
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            # Преобразовать JSON в список словарей
+            data = response.json()
+
+            return data
+        else:
+            print(f"Ошибка при получении данных: {response.status_code}")
+            return []
+
+    except requests.exceptions.RequestException as e:
+        print(f"Произошла ошибка при выполнении запроса: {e}")
+        return []
+
+
+# Вызов функции для получения списка всех криптовалют
+all_coins = get_all_coins()
+
+# Вывод результатов (пример)
+if all_coins:
+    print('Список первых 10 из всех криптовалют')
+    for coin in all_coins[:10]:  # Вывод только первых 10 монет
+        print(f"Идентификатор: {coin['id']}, Название: {coin['name']}, Символ: {coin['symbol']}")
+
+
 # Создание графического интерфейса
 window = Tk()
 window.title("Курс обмена валюты")
